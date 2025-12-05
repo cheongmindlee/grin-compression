9bac05650a723f9744e63c1aa8b91eb83f73b0a1
David Lee <Cheongmin.gitlee@gmail.com>
2025-12-05 12:12:33 -0600

    Implemented encode in Grin.java and createFrequencyMap

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Authors: (_TODO: fill in names here!_)
 
 ## Resources
 
-*   (_TODO: fill in resources here!_)
-*   ...
-*   ...
+- I used stack overflow to better understand the functions that come with the Map interface
+- (https://stackoverflow.com/questions/46898/how-do-i-efficiently-iterate-over-each-entry-in-a-java-map)
+- ...
+- ...
 
 ## Revision Log
 
-*   (_TODO: fill in your changes to your project here!_)
+- (_TODO: fill in your changes to your project here!_)
--- src/main/java/edu/grinnell/csc207/compression/Grin.java
+++ src/main/java/edu/grinnell/csc207/compression/Grin.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,26 +15,27 @@ public class Grin {
     /**
      * Decodes the .grin file denoted by infile and writes the output to the
      * .grin file denoted by outfile.
-     * @param infile the file to decode
+     * 
+     * @param infile  the file to decode
      * @param outfile the file to ouptut to
      */
-    public static void decode (String infile, String outfile) {
-        try{
+    public static void decode(String infile, String outfile) {
+        try {
             BitInputStream in = new BitInputStream(infile);
             BitOutputStream out = new BitOutputStream(outfile);
 
-            //Check the magic number is a grinfile
+            // Check the magic number is a grinfile
             int magic = in.readBits(32);
-            if(magic != 1846){
-                throw new IllegalArgumentException("invalid file type");
+            if (magic != 1846) {
+                throw new IllegalArgumentException("Incorrect magic number");
             }
 
-            //Construct a Huffmantree from the infile
+            // Construct a Huffmantree from the infile
             HuffmanTree tree = new HuffmanTree(in);
 
-            //Decode the text
+            // Decode the text
             tree.decode(in, out);
-        } catch(IOException e){
+        } catch (IOException e) {
             throw new IllegalArgumentException("There was an error reading the bitstream", e);
         }
 
@@ -43,55 +45,69 @@ public class Grin {
      * Creates a mapping from 8-bit sequences to number-of-occurrences of
      * those sequences in the given file. To do this, read the file using a
      * BitInputStream, consuming 8 bits at a time.
+     * 
      * @param file the file to read
      * @return a freqency map for the given file
      */
-    public static Map<Short, Integer> createFrequencyMap (String file) {
-        // TODO: fill me in!
-        return null;
+    public static Map<Short, Integer> createFrequencyMap(String file) {
+        // Create a frequency map
+        Map<Short, Integer> frequencyMap = new HashMap<>();
+        try {
+            BitInputStream inStream = new BitInputStream(file);
+
+            // Hold the 8 bits read from the file
+            int bits;
+            // Read the file and create the frequency map
+            while ((bits = inStream.readBits(8)) != -1) {
+                short key = (short) bits;
+                if (frequencyMap.containsKey(key)) {
+                    frequencyMap.put(key, frequencyMap.get(key) + 1);
+                } else {
+                    frequencyMap.put(key, 1);
+                }
+            }
+            // Close the stream once it is used
+            inStream.close();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("There was an error with the BitInputStream", e);
+        }
+
+        return frequencyMap;
     }
 
     /**
      * Encodes the given file denoted by infile and writes the output to the
      * .grin file denoted by outfile.
-     * @param infile the file to encode.
+     * 
+     * @param infile  the file to encode.
      * @param outfile the file to write the output to.
      */
-    public static void encode(String infile, String outfile) throws IOException{
-        Map<Short, Integer> freq;
-        Path path = Path.of(infile);
-        String text = Files.readString(path);
-
-        List<String> words = Arrays.asList(text.split("\\R"));
-        for(String word: words){
-            freq.get()
-            freq.put(Integer.parseInt(word), );
-            
-        }
+    public static void encode(String infile, String outfile) throws IOException {
+        // Create a frequency map from the input file
+        Map<Short, Integer> frequencyMap = createFrequencyMap(infile);
 
     }
 
     /**
      * The entry point to the program.
+     * 
      * @param args the command-line arguments.
      */
     public static void main(String[] args) {
         System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
-        //Make sure there are correct user inputs
-        if(args.length != 3){
+        // Make sure there are correct user inputs
+        if (args.length != 3) {
             System.out.println("Please make sure your input is of form <encode|decode> <infile> <outfile>");
             return;
-        } 
-        //Make sure there were 
+        }
+        // Make sure there were
         String input = args[0];
         String inFile = args[1];
         String outFile = args[2];
 
-        if(input.equals("decode")){
+        if (input.equals("decode")) {
             decode(inFile, outFile);
-            System.out.println("This ran");
-        } else{
-            System.out.println("This is not working");
+        } else {
         }
     }
 }
